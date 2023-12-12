1e603f333994db4cdb00a5df3ce9c11b2e4cdab5
Thumbs-up-emoji <119483728+Thumbs-up-emoji@users.noreply.github.com>
2023-12-12 16:52:12 +0530

    Auto-opens URL

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
+import time
+import webbrowser
 from flask import Flask
 from views import views
+import subprocess
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/views")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    # Run the app in a separate process
+    process = subprocess.Popen(["python", "-m", "flask", "run", "--no-reload", "--port=8000"])
 
+    # Wait in case of unexpected lag
+    time.sleep(1)
 
+    # Open the URL in the default web browser
+    webbrowser.open("http://127.0.0.1:8000/views/")
+
+    try:
+        # Wait for the user to press Ctrl+C
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        # Stop the Flask server
+        process.terminate()
